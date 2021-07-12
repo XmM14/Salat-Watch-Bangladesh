3b2e278f4a6879ec1ae0394bb8b39febdcf4206f
XmM14 <wzfnafis@gmail.com>
2021-07-13 01:11:30 +0600

    Start Coding

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
+#include<stdio.h>
 int main ()
 {
+    char state[20];
+    char district[20];
+    
+    {
+        printf("                Salat Watch Bangladesh              \n\n");
+        printf("                    Plz Set Up                      \n\n");
+        printf("State : ");
+        scanf("%s", state);
+        printf("District : ");
+        printf("Your Location is : %s, %s", district, state);
+    }
+    
     return 0;
 }
