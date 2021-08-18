bdad07bb1a082e56ae6faec4892aeecc618dbc53
XmM14 <wzfnafis@gmail.com>
2021-08-18 15:04:02 +0600

    create two new function for no of months and get_searched date. Then display salat time for searched date.

--- main.c
+++ main.c
@@ -4,6 +4,16 @@
 #include <ctype.h>
 #include <time.h>
 
+const int year = 2021;
+const int DAYS_IN_2021 = 365;
+const int number_of_states = 8;
+const int number_of_months = 12;
+
+typedef struct prayer_time
+{
+    char date[20], day[10], fajr[10], shuruq[10], dhuha[10], dhuhur[10], asr[10], magribh[10], isha[10], qibla_hour[10];
+} prayer_time;
+
 char *take_state_from_user();
 int is_valid_state(char *state, char *valid_states[], int number_of_state);
 
@@ -11,6 +21,16 @@ char *take_district_from_user();
 int is_valid_district(char *district, char *valid_districts[], int number_of_districts);
 
 void show_current_time();
+
+prayer_time *get_list_of_prayer_time(char *path);
+
+char *get_current_date();
+
+char *get_searched_date(int month, int date);
+
+prayer_time search_prayer_time(prayer_time *list_of_prayer_time, char *a_date);
+
+void display_salat_times(prayer_time prayer_time_of_searched_day);
 
 char *take_month_from_user();
 int is_valid_month(char *valid_months[], char *month, int number_of_months);
@@ -20,6 +40,9 @@ int is_valid_date(int *valid_dates, int date, int number_of_date);
 
 int days_in_month(char *months_with_31_days[], char *months_with_30_days[], int *number_of_months_in_type_of_months,
                   char *month);
+
+int no_of_months(char *valid_months[], char *month, int number_of_months);
+
 int index_of(char *valid_states[], char *sate, int number_of_states);
 
 char *strupr(char *s);
@@ -29,16 +52,23 @@ int main()
     system("COLOR 6");
 
     int choice = 1;
-    int number_of_states = 8;
-    int number_of_months = 12;
     int number_of_days;
+    int months_no;
 
     char *state;
     char *district;
     char *month;
     int date;
-
+    char *curr_date;
+    char *searched_date;
+    char full_date[10];
     int index_of_state;
+
+    prayer_time *list_of_prayer_time;
+    prayer_time prayer_time_of_searched_day;
+
+    char path[200] = "D:\\XmM\\Salat-Watch-Bangladesh\\";
+
     char *valid_states[20] = {"DHAKA", "CHATTOGRAM", "BARISAL", "KHULNA",
                               "RAJSHAHI", "RANGPUR", "SYLHET", "MYMENSINGH"};
 
@@ -70,7 +100,7 @@ int main()
 
     int number_of_districts_in_state[10] = {13, 11, 6, 10, 8, 8, 4, 4};
 
-    printf("\n\t\t\t\t|| Welcome to Salat Watch Bangladesh 2021 ||\n\n");
+    printf("\n\t\t\t|| Welcome to Salat Watch Bangladesh 2021 ||\n\n");
 
     while (choice)
     {
@@ -97,11 +127,21 @@ int main()
             district = take_district_from_user();
         } while (is_valid_district(district, valid_districts[index_of_state], number_of_districts_in_state[index_of_state]) == 0);
 
-        printf("\nYour Location is (%s, %s)", state, district);
+        printf("\nYou set your Location : (%s, %s)", state, district);
+        printf("\nPrayer Times in %s, %s Today - ", state, district);
 
-        show_current_time();
+        curr_date = get_current_date();
+        printf("%s", curr_date);
 
-        //After successfully input location programm will show salat time of present date
+        //show_current_time(); //we can print next and present prayer comparing with current time;
+
+        strcat(path, district);
+        strcat(path, ".txt");
+
+        list_of_prayer_time = get_list_of_prayer_time(path);
+        prayer_time_of_searched_day = search_prayer_time(list_of_prayer_time, curr_date);
+
+        display_salat_times(prayer_time_of_searched_day);
 
         printf("\n\nDo You want to see Salat time of Any other day?\n");
         printf("If Yes(Press 1), If No(Press 0): ");
@@ -137,20 +177,25 @@ int main()
                 date = take_date_from_user();
             } while (is_valid_date(valid_dates, date, number_of_days) == 0);
 
-            printf("\n\n\t\t\t || Salat time of %s %d  ||", month, date);
+            months_no = no_of_months(valid_months, month, number_of_months);
+            searched_date = get_searched_date(months_no, date);
+            
+            list_of_prayer_time = get_list_of_prayer_time(path);
+            prayer_time_of_searched_day = search_prayer_time(list_of_prayer_time, searched_date);
 
-            //after successfully inputed month and date program will show time of salat of serched date
+            printf("\n\n\nSalat time of %s, %d, 2021", month, date);
+            display_salat_times(prayer_time_of_searched_day);
 
             printf("\n\nDo You want to see Salat time of Any other day?\n");
             printf("If Yes(Press 1), If No(Press 0): ");
             scanf("%d", &choice);
-
         }
 
         printf("\n\nSetup again (Press 1) or exit (press 0)   : ");
         scanf("%d", &choice);
     }
     printf("\n\n\t\t\tThanks For All your Patience!");
+
     return 0;
 }
 
@@ -185,7 +230,7 @@ char *take_district_from_user()
     char *district;
     district = (char *)malloc(sizeof(20));
 
-    printf("\nDistrict: ");
+    printf("\n\nDistrict: ");
     scanf("%s", district);
     strupr(district);
     return district;
@@ -212,24 +257,127 @@ void show_current_time()
 
     time(&now);
 
+    struct tm *local = localtime(&now);
+
+    hours = local->tm_hour;
+    minutes = local->tm_min;
+    seconds = local->tm_sec;
+
+    days = local->tm_mday;
+    months = local->tm_mon + 1;
+    years = local->tm_year + 1900;
+
+    if (hours < 12)
+    {
+        printf("\nThe time is now : %02d:%02d:%02d am\n", hours, minutes, seconds);
+    }
+    else
+    {
+        printf("\nThe time is now : %02d:%02d:%02d pm\n", hours + 12, minutes, seconds);
+    }
+}
+
+prayer_time *get_list_of_prayer_time(char *path)
+{
+    FILE *prayer_time_file;
+
+    prayer_time_file = fopen(path, "r");
+
+    char line[200];
+
+    prayer_time *list_of_prayer_time;
+    list_of_prayer_time = (prayer_time *)malloc(sizeof(prayer_time) * 400);
+
+    int index = 0;
+
+    while (fgets(line, 1000, prayer_time_file) != NULL)
+    {
+
+        sscanf(line, "%s %s %s %s %s %s %s %s %s %s",
+               list_of_prayer_time[index].date,
+               list_of_prayer_time[index].day,
+               list_of_prayer_time[index].fajr,
+               list_of_prayer_time[index].shuruq,
+               list_of_prayer_time[index].dhuha,
+               list_of_prayer_time[index].dhuhur,
+               list_of_prayer_time[index].asr,
+               list_of_prayer_time[index].magribh,
+               list_of_prayer_time[index].isha,
+               list_of_prayer_time[index].qibla_hour);
+
+        index += 1;
+    }
+
+    fclose(prayer_time_file);
+    return list_of_prayer_time;
+}
+
+char *get_current_date()
+{
+    char *current_day;
+    current_day = (char *)malloc(sizeof(char) * 50);
+
+    int hours, minutes, seconds, days, months, years;
+
+    time_t now;
+
+    time(&now);
 
     struct tm *local = localtime(&now);
 
-    hours = local->tm_hour;  
-    minutes = local->tm_min; 
-    seconds = local->tm_sec; 
+    hours = local->tm_hour;
+    minutes = local->tm_min;
+    seconds = local->tm_sec;
 
-    days = local->tm_mday;         
-    months = local->tm_mon + 1;    
-    years = local->tm_year + 1900; 
+    days = local->tm_mday;
+    months = local->tm_mon + 1;
+    years = local->tm_year + 1900;
 
-    if (hours < 12)
-    { 
-        printf("\n\nToday is : %02d/%02d/%d  %02d:%02d:%02d am\n", days, months, years, hours, minutes, seconds);
+    sprintf(current_day, "%02d/%02d/%d", days, months, years);
+    return current_day;
+}
+
+char *get_searched_date(int month, int date)
+{
+    char *searched_day;
+    searched_day = (char *)malloc(sizeof(char) * 50);
+
+    sprintf(searched_day, "%02d/%02d/%d", date, month, year);
+    return searched_day;
+}
+
+prayer_time search_prayer_time(prayer_time *list_of_prayer_time, char *a_date)
+{
+    int i;
+
+    for (i = 0; i < DAYS_IN_2021; i += 1)
+    {
+        if (strcmp(list_of_prayer_time[i].date, a_date) == 0)
+        {
+            return list_of_prayer_time[i];
+        }
+    }
+}
+
+void display_salat_times(prayer_time prayer_time_of_searched_day)
+{
+    printf("\n\n\tDate        %s", prayer_time_of_searched_day.date);
+    printf("\n\tDay          %s", prayer_time_of_searched_day.day);
+    printf("\n\tFajr         %s", prayer_time_of_searched_day.fajr);
+    printf("\n\tshuruq       %s", prayer_time_of_searched_day.shuruq);
+    printf("\n\tDhuha        %s", prayer_time_of_searched_day.dhuha);
+    printf("\n\tDhuhur       %s", prayer_time_of_searched_day.dhuhur);
+    printf("\n\tAsr          %s", prayer_time_of_searched_day.asr);
+    printf("\n\tMagribh      %s", prayer_time_of_searched_day.magribh);
+    printf("\n\tIsha         %s", prayer_time_of_searched_day.isha);
+
+    if (prayer_time_of_searched_day.qibla_hour == NULL)
+    {
+        printf("\n\tQibla Hour        not available");
     }
     else
-    { 
-        printf("\n\nToday is : %02d/%02d/%d  %02d:%02d:%02d pm\n", days, months, years, hours, minutes, seconds);
+    {
+        printf("\n\tQibla Hour   %s", prayer_time_of_searched_day.qibla_hour);
     }
 }
 
@@ -275,6 +423,17 @@ int days_in_month(char *months_with_31_days[], char *months_with_30_days[],
             return 30;
     }
     return 28;
+}
+
+int no_of_months(char *valid_months[], char *month, int number_of_months)
+{
+    for (int i = 0; i < number_of_states; i += 1)
+    {
+        if (strcmp(valid_months[i], month) == 0)
+        {
+            return i + 1;
+        }
+    }
 }
 
 int take_date_from_user()
